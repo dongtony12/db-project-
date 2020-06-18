ec697bf1cb3698ddbf2653654783db9885cc4ef9
dongtony <dongtony0523@naver.com>
2020-06-19 04:34:11 +0900

    tlqkf2

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   //res.render('index', { title: 'Express' }); 기존코드
-  console.log("========무비 인덱스===========");
+  console.log("===================");
   res.send(movies);
   res.sendFile(path.join(__dirname, '../public', 'index.html'))
 });
